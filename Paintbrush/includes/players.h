29f4f9d5d6529f63fd706441322ade802c11fead
requiem958 <requiemandsahariel@gmail.com>
2018-08-31 09:49:48 +0200

    Modifications to erase the old Painter struct

--- Paintbrush/includes/players.h
+++ Paintbrush/includes/players.h
@@ -12,9 +12,48 @@
 #include "types.h"
 
 player* createPlayer();
+
+void setSpriteRight(player*,SDL_Texture*);
+void setSpriteLeft(player*,SDL_Texture*);
+void setActiveSprite(player*,SDL_Texture**);
+void setPosition(player*,const int, const int);
+void setBrushSize(player*,const BrushSize);
+void setCtrl(player*,const int,const SDL_Keycode);
+void setCodedColor(player*,const Uint32);
+void setStructColor(player*,const Uint8 ,const Uint8 ,const Uint8 , const Uint8 );
+
+void setDef(player*,const SDL_bool);
+void setName(player*,const char*,const unsigned int);
+void setId(player*,const char);
+void setScore(player*,const double);
+void setSpeedX(player*,const Speed);
+void setSpeedY(player*,const Speed);
+void setSpeed(player*,const Speed,const Speed);
 void setDirectionX(player*,const Direction);
 void setDirectionY(player*,const Direction);
 void setDirection(player*,const Direction,const Direction);
+
+SDL_Texture* getSpriteRight(const player*);
+SDL_Texture* getSpriteLeft(const player*);
+SDL_Texture* getActiveSprite(const player*);
+int getX(const player*);
+int getY(const player*);
+SDL_Keycode getCtrlRight(const player*);
+SDL_Keycode getCtrlLeft(const player*);
+SDL_Keycode getCtrlDown(const player*);
+SDL_Keycode getCtrlUp(const player*);
+Uint32 getCodedColor(const player*);
+SDL_Color getStructColor(const player*);
+
+SDL_bool isDef(const player*);
+char* getName(const player*);
+char getId(const player*);
+double getScore(const player*);
+Speed getXSpeed(const player*);
+Speed getYSpeed(const player*);
+Direction getXDir(const player*);
+Direction getYDir(const player*);
+
 void movePlayer(player*);
 int deletePlayer(player* play);
 int deleteAllPlayers();
--- Paintbrush/src/players.c
+++ Paintbrush/src/players.c
@@ -11,225 +11,235 @@
 extern GameData* data;
 extern player* players[];
 
-
-static painter* allocPainter( void )
-{
-    painter* testing = NULL;
-    testing = malloc( sizeof( painter ) );
-    return testing;
-}
-
-static void initPainter(painter* paint)
-{
-    if(paint != NULL)
-    {
-        paint->activeSprit = NULL;
-        paint->brushSize = BRUSH_NORMAL;
-        paint->position.h =0;
-        paint->position.w =0;
-        paint->position.x =0;
-        paint->position.y =0;
-    }
-}
-
-static painter* createSkeletonPainter()
-{
-    painter* paint = NULL;
-
-    paint = allocPainter();
-    if(paint != NULL)
-        initPainter(paint);
-    else
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Memory error (painter alloc)");
-    return paint;
-}
-
-static void setColor( painter* p, const Uint32 c )
-{
-    if(p != NULL)
-    {
-        p->color = c;
-    }
-}
-
-
-static void setBrushSize(painter* p, const BrushSize size)
-{
-    if(p != NULL)
-    {
-        p->brushSize = size;
-    }
-}
-
-static void setPosition( painter* p, const int x, const int y )
-{
-    if( p != NULL )
-    {
-        if(x != -1)
-            p->position.x = x;
-        if(y != -1)
-            p->position.y = y;
-    }
-}
-
-static void setCtrlKeys(painter* p,
-                const SDL_Keycode key_right, const SDL_Keycode key_left,
-                const SDL_Keycode key_up, const SDL_Keycode key_bottom)
-{
-    if(p != NULL)
-    {
-        p->keys[CTRL_KEY_RIGHT] = key_right;
-        p->keys[CTRL_KEY_LEFT]  = key_left;
-        p->keys[CTRL_KEY_DOWN]  = key_bottom;
-        p->keys[CTRL_KEY_UP]    = key_up;
-    }
-}
-
-static int deletePainter( painter* p )
-{
-    if( p != NULL )
-    {
-      /** \todo Use native painter functions */
-        p->activeSprit  = NULL;
-        p->position.h =0;
-        p->position.w =0;
-        p->position.x =0;
-        p->position.y =0;
-        p->keys[0] = 0;
-        p->keys[1] = 0;
-        p->keys[2] = 0;
-        p->keys[3] = 0;
-        p->brushSize =0;
-        p->color = 0;
-        if( p->spriteLeft != NULL )
-            SDL_DestroyTexture( p->spriteLeft );
-        if( p->spriteRight != NULL )
-            SDL_DestroyTexture( p->spriteRight );
-        free( p );
-    }
-    if( p==NULL )
-        return 0;
-    return -1;
-}
+/* Allocator */
 
 static player* allocPlayer()
 {
-    player* temp;
-    temp = malloc(sizeof(player));
-    if(temp == NULL)
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Memory error");
-    return temp;
+  player* temp;
+  temp = malloc(sizeof(player));
+  if(temp == NULL)
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Memory error");
+  return temp;
 }
+
+/* Setters */
+
+void setSpriteRight(player* p, SDL_Texture* t)
+{
+  if (p != NULL)
+    {
+      p->spriteRight = t;
+    }
+}
+
+void setSpriteLeft(player* p, SDL_Texture* t)
+{
+  if (p != NULL)
+    {
+      p->spriteLeft = t;
+    }
+}
+
+void setActiveSprite(player* p, SDL_Texture** t)
+{
+  if (p != NULL)
+    {
+      if (p->activeSprite == t)
+	;
+      else if (*t == getSpriteRight(p) || *t == getSpriteLeft(p))
+	p->activeSprite = t;
+      else
+	SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Attempt to defigurate player (%s)", getName(p));
+    }
+}
+
+void setPosition( player* p, const int x, const int y )
+{
+  if( p != NULL )
+    {
+      if(x != -1)
+	p->position.x = x;
+      if(y != -1)
+	p->position.y = y;
+    }
+}
+
+void setBrushSize(player* p, const BrushSize size)
+{
+  if(p != NULL)
+    {
+      p->size = size;
+      p->position.h = p->position.w = size;
+    }
+}
+
+void setCtrl(player *p, const int index, const SDL_Keycode key)
+{
+  if(p != NULL)
+    {
+      if (index >= 0 && index < 4)
+	p->keys[index] = key;
+      else
+	SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Forbidden key index: %d", index);
+    }
+}
+
+void setCodedColor( player* p, const Uint32 c )
+{
+  if(p != NULL)
+    {
+      p->color = c;
+    }
+}
+
+void setStructColor( player* p, const Uint8 r, const Uint8 g, const Uint8 b, const Uint8 a )
+{
+  if(p != NULL)
+    {
+      p->struct_color.r = r;
+      p->struct_color.g = g;
+      p->struct_color.b = b;
+      p->struct_color.a = a;
+    }
+}
+
+void setDef(player* p, const SDL_bool b)
+{
+  if (p != NULL)
+    p->isdef = b;
+}
+
+void setName(player* p, const char* name, const unsigned int size)
+{
+  if (p != NULL)
+    {
+      if (size > 0 && size < NAME_STRING_MAX)
+	strcpy(p->name,name);
+      else
+	SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Name out of range (length) : %s", name);
+    }
+}
+
+void setId(player* p, const char id)
+{
+  if (p != NULL)
+    {
+      if (id >= 0 && id < MAX_PLAYERS)
+	p->id = id;
+      else
+	SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Forbidden id index: %d ; %s", id, getName(p));
+    }
+}
+
+void setScore(player* p, double score)
+{
+  if(p != NULL)
+    {
+      p->scoreP = score;
+    }
+}
+
+void setSpeed(player* p, Speed sx, Speed sy)
+{
+  if (p != NULL)
+    {
+      p->speed.x = sx;
+      p->speed.y = sy;
+    }
+}
+
+void setDirectionX( player* p, const Direction dx )
+{
+  if(p != NULL)
+    {
+      if( p->dir.x == dx )
+	p->dir.y = DIR_STOP;
+      p->dir.x = dx;
+    }
+}
+
+void setDirectionY( player* p, const Direction dy )
+{
+  if(p != NULL)
+    {
+      if( p->dir.y == dy )
+	p->dir.x = DIR_STOP;
+      p->dir.y = dy;
+    }
+}
+
+void setDirection( player* p, const Direction dx, const Direction dy)
+{
+  if(p != NULL)
+    {
+      setDirectionX(p, dx);
+      setDirectionY(p, dy);
+    }
+}
+
+/*Initialisation*/
 
 static int initPlayer(player* p)
 {
   int ret = EXIT_SUCCESS;
 
   if(p == NULL)
-  {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Player isn't allocated");
-    ret = EXIT_FAILURE;
-  }
-  else if( p->isdef == SDL_TRUE )
-  {
-    SDL_LogError( SDL_LOG_CATEGORY_APPLICATION,
-		  "Player %s is almost allocated, will ignore it", p->name );
-  }
-  else
-  {
-    p->isdef = SDL_TRUE;
-    p->scoreP = 0.0;
-
-    p->Painter = createSkeletonPainter();
-    if(p->Painter == NULL)
     {
-      SDL_LogError( SDL_LOG_CATEGORY_APPLICATION,
-		    "Can't allocate painter for Player %s", p->name );
-      deletePlayer(p);
+      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Player isn't allocated");
       ret = EXIT_FAILURE;
     }
-  }
+  else if( p->isdef == SDL_TRUE )
+    {
+      SDL_LogError( SDL_LOG_CATEGORY_APPLICATION,
+		    "Player %s is almost allocated, will ignore it", p->name );
+    }
+  else
+    {
+      setDef(p,SDL_TRUE);
+      setScore(p,0.0);
+      setSpriteRight(p,NULL);
+      setSpriteLeft(p,NULL);
+      setActiveSprite(p,getSpriteRight(p));
+      setPosition(p,0,0);
+      setBrushSize(p,BRUSH_NORMAL);
+    }
   return ret;
 }
 
-void setDirectionX( player* p, const Direction dx )
-{
-    if(p != NULL)
-    {
-        if( p->dir.x == dx )
-            p->dir.y = DIR_STOP;
-        p->dir.x = dx;
-    }
-}
-
-void setDirectionY( player* p, const Direction dy )
-{
-    if(p != NULL)
-    {
-        if( p->dir.y == dy )
-            p->dir.x = DIR_STOP;
-        p->dir.y = dy;
-    }
-}
-
-void setDirection( player* p, const Direction dx, const Direction dy)
-{
-    if(p != NULL)
-    {
-        setDirectionX(p, dx);
-        setDirectionY(p, dy);
-    }
-}
+/*Destructor */
 
 int deletePlayer( player* play )
 {
-    int ret = EXIT_SUCCESS;
-    if( play != NULL)
+  int ret = EXIT_SUCCESS;
+  if( play != NULL)
     {
-        if( play->isdef == SDL_TRUE )
+      if( play->isdef == SDL_TRUE )
         {
-            if( play->Painter != NULL )
-                deletePainter( play->Painter );
-            else
-            {
-                SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
-                             "Sub Painter of player (%s) isn't allocated, it's not normally.", play->name );
-                ret = EXIT_FAILURE;
-            }
-	    play->scoreP = 0.0;
-	    play->isdef = SDL_FALSE;
+	  if (p->spriteLeft != NULL)
+	    SDL_DestroyTexture(p->spriteLeft);
+	  if (p->spriteRight != NULL)
+	    SDL
         }
-        else
+      else
         {
-            SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
-                         "Player isn't defined, he's probably not still created or some errors happened" );
-            ret = EXIT_FAILURE;
+	  SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
+		       "Player isn't defined, he's probably not still created or some errors happened" );
+	  ret = EXIT_FAILURE;
         }
-	free(play);
+      free(play);
     }
-    else
+  else
     {
-        SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
-                        "Player is not allocated");
-        ret = EXIT_FAILURE;
+      SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
+		   "Player is not allocated");
+      ret = EXIT_FAILURE;
     }
-    return ret;
+  return ret;
 }
 
-void setScore(player* p, double score)
-{
-    if(p != NULL)
-    {
-        p->scoreP = score;
-    }
-}
+/*High level Functions */
 
-void setSpeed(player* p, Speed sx, Speed sy){
-  if (p != NULL){
-    p->speed.x = sx;
-    p->speed.y = sy;
-  }
-}
+
 
 /** \fn player* createPlayer()
  *  \brief Function that create a player in interaction with user
@@ -239,124 +249,124 @@ void setSpeed(player* p, Speed sx, Speed sy){
  */
 player* createPlayer()
 {
-    static unsigned char actual_id = 0;
-    player* p = NULL;
-    SDL_Color* color = NULL;
+  static unsigned char actual_id = 0;
+  player* p = NULL;
+  SDL_Color* color = NULL;
 
-    if( (p = allocPlayer()) != NULL)
+  if( (p = allocPlayer()) != NULL)
     {
-        if (initPlayer(p) != EXIT_FAILURE)
+      if (initPlayer(p) != EXIT_FAILURE)
         {
-        p->id = actual_id;
-        color = &(p->Painter->struct_color);
-        printf( "Please enter a name for that young player (31 letters max):" );
-        readline( p->name, NAME_STRING_MAX );
-        printf( "Now type the three (r,g,b) components of his color :" );
-        scanf( "%3u %3u %3u", &color->r,&color->g,&color->b );
-        CLEAR_BUFFER;
-        if  ( color->r + color->g + color->b <= ( UINT8_MAX*3 ))
-        {
-            if( data->pixelformat != NULL )
-            {
-                color->a = SDL_ALPHA_OPAQUE;
-                setColor(p->Painter,SDL_MapRGB(data->pixelformat,color->r,color->g,color->b));
-                setBrushSize(p->Painter, BRUSH_NORMAL);
-                setSpeed(p, SPEED_AVERAGE, SPEED_AVERAGE);
-                setDirection(p, DIR_STOP, DIR_STOP);
-                setScore(p, 0.0);
+	  p->id = actual_id;
+	  color = &(p->Painter->struct_color);
+	  printf( "Please enter a name for that young player (31 letters max):" );
+	  readline( p->name, NAME_STRING_MAX );
+	  printf( "Now type the three (r,g,b) components of his color :" );
+	  scanf( "%3u %3u %3u", &color->r,&color->g,&color->b );
+	  CLEAR_BUFFER;
+	  if  ( color->r + color->g + color->b <= ( UINT8_MAX*3 ))
+	    {
+	      if( data->pixelformat != NULL )
+		{
+		  color->a = SDL_ALPHA_OPAQUE;
+		  setColor(p->Painter,SDL_MapRGB(data->pixelformat,color->r,color->g,color->b));
+		  setBrushSize(p->Painter, BRUSH_NORMAL);
+		  setSpeed(p, SPEED_AVERAGE, SPEED_AVERAGE);
+		  setDirection(p, DIR_STOP, DIR_STOP);
+		  setScore(p, 0.0);
 
-                switch(p->id)
-                {
+		  switch(p->id)
+		    {
                     case 0:
-                        setPosition(p->Painter,    (data->window_width/4),
-                                                        (data->window_height/4) );
-                        setCtrlKeys(p->Painter, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN);
-                        break;
+		      setPosition(p->Painter,    (data->window_width/4),
+				  (data->window_height/4) );
+		      setCtrlKeys(p->Painter, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN);
+		      break;
                     case 1:
-                        setPosition(p->Painter,    3*(data->window_width/4),
-                                                        (data->window_height/4) );
-                        setCtrlKeys(p->Painter, SDLK_d, SDLK_q ,SDLK_z ,SDLK_s);
-                        break;
+		      setPosition(p->Painter,    3*(data->window_width/4),
+				  (data->window_height/4) );
+		      setCtrlKeys(p->Painter, SDLK_d, SDLK_q ,SDLK_z ,SDLK_s);
+		      break;
                     case 2:
-                        setPosition(p->Painter,    (data->window_width/4),
-                                                        (data->window_height/4) );
-                        setCtrlKeys(p->Painter, SDLK_m, SDLK_k, SDLK_o, SDLK_l);
-                        break;
+		      setPosition(p->Painter,    (data->window_width/4),
+				  (data->window_height/4) );
+		      setCtrlKeys(p->Painter, SDLK_m, SDLK_k, SDLK_o, SDLK_l);
+		      break;
                     case 3:
-                        setPosition(p->Painter,    3*(data->window_width/4),
-                                                        3*(data->window_height/4) );
-                        setCtrlKeys(p->Painter, SDLK_6, SDLK_4, SDLK_8, SDLK_5);
-                        break;
+		      setPosition(p->Painter,    3*(data->window_width/4),
+				  3*(data->window_height/4) );
+		      setCtrlKeys(p->Painter, SDLK_6, SDLK_4, SDLK_8, SDLK_5);
+		      break;
                     default:
-                        SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
-                                    "Actual id is invalid : %d", actual_id);
-                        setPosition(p->Painter, 42,42);
-                        setCtrlKeys(p->Painter, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN);
-                        break;
+		      SDL_LogInfo( SDL_LOG_CATEGORY_APPLICATION,
+				   "Actual id is invalid : %d", actual_id);
+		      setPosition(p->Painter, 42,42);
+		      setCtrlKeys(p->Painter, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN);
+		      break;
 
-                }
+		    }
 
-                p->Painter->spriteLeft = IMG_LoadTexture( *data->renderer,
-                                              "pics/paintbrush_toleft.png" );
-                p->Painter->spriteRight = IMG_LoadTexture( *data->renderer,
-                                               "pics/paintbrush_toright.png" );
-                SDL_QueryTexture( p->Painter->spriteLeft, NULL, NULL,
-                                  &( p->Painter->position.w ), &( p->Painter->position.h ) );
-                p->Painter->activeSprit = &p->Painter->spriteLeft;
-            }
-        }
+		  p->Painter->spriteLeft = IMG_LoadTexture( *data->renderer,
+							    "pics/paintbrush_toleft.png" );
+		  p->Painter->spriteRight = IMG_LoadTexture( *data->renderer,
+							     "pics/paintbrush_toright.png" );
+		  SDL_QueryTexture( p->Painter->spriteLeft, NULL, NULL,
+				    &( p->Painter->position.w ), &( p->Painter->position.h ) );
+		  p->Painter->activeSprit = &p->Painter->spriteLeft;
+		}
+	    }
+	}
     }
-    }
-    /* Creation Phase end  */
-    actual_id = (actual_id != data->numberOfPlayer) ? (actual_id + 1) : (0);
-    return p;
+  /* Creation Phase end  */
+  actual_id = (actual_id != data->numberOfPlayer) ? (actual_id + 1) : (0);
+  return p;
 }
 
 void movePlayer( player* p )
 {
-    if( p==NULL );
-    else
+  if( p==NULL );
+  else
     {
-        /* Update player position*/
-        p->Painter->position.y += ( p->speed.y )*( p->dir.y );
-        p->Painter->position.x += ( p->speed.x )*( p->dir.x );
-        /* RIGHT */
-        if( p->Painter->position.x >= ( signed )( data->window_width - p->Painter->position.w ) )
+      /* Update player position*/
+      p->Painter->position.y += ( p->speed.y )*( p->dir.y );
+      p->Painter->position.x += ( p->speed.x )*( p->dir.x );
+      /* RIGHT */
+      if( p->Painter->position.x >= ( signed )( data->window_width - p->Painter->position.w ) )
         {
-            setPosition( p->Painter, data->window_width- p->Painter->position.w, -1 );
+	  setPosition( p->Painter, data->window_width- p->Painter->position.w, -1 );
         }
-        /* LEFT */
-        else if( p->Painter->position.x < ( signed )0 )
+      /* LEFT */
+      else if( p->Painter->position.x < ( signed )0 )
         {
-            setPosition( p->Painter, 0, -1 );
+	  setPosition( p->Painter, 0, -1 );
         }
-        /* DOWN */
-        if( p->Painter->position.y >= ( signed )( data->window_height - p->Painter->position.h ) )
+      /* DOWN */
+      if( p->Painter->position.y >= ( signed )( data->window_height - p->Painter->position.h ) )
         {
-            setPosition(p->Painter, -1, data->window_height - p->Painter->position.h);
+	  setPosition(p->Painter, -1, data->window_height - p->Painter->position.h);
         }
-        /* HIGH */
-        else if( p->Painter->position.y < 0 )
-            setPosition(p->Painter, -1, 0);
+      /* HIGH */
+      else if( p->Painter->position.y < 0 )
+	setPosition(p->Painter, -1, 0);
     }
 }
 
 int deleteAllPlayers()
 {
-    int ret = EXIT_SUCCESS;
-    while( data->numberOfPlayer-- >0 )
+  int ret = EXIT_SUCCESS;
+  while( data->numberOfPlayer-- >0 )
     {
-        if( players[data->numberOfPlayer]->isdef
-                && players[data->numberOfPlayer]->Painter != NULL )
+      if( players[data->numberOfPlayer]->isdef
+	  && players[data->numberOfPlayer]->Painter != NULL )
         {
-            if( deletePlayer( players[data->numberOfPlayer] ) != EXIT_SUCCESS )
+	  if( deletePlayer( players[data->numberOfPlayer] ) != EXIT_SUCCESS )
             {
-                SDL_LogError( SDL_LOG_CATEGORY_ERROR,
-                              "During player deleting (see above)" );
-                ret = EXIT_FAILURE;
+	      SDL_LogError( SDL_LOG_CATEGORY_ERROR,
+			    "During player deleting (see above)" );
+	      ret = EXIT_FAILURE;
             }
         }
     }
 
-    return ret;
+  return ret;
 }
